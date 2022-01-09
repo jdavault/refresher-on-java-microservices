5c6cd15e2811eb3a33a657ba58e9025cb2e69e17
Joe Davault <joe.davault@gmail.com>
2022-01-08 21:46:20 -0700

    adding property filtering with SimpleBeanPropertyFilter

--- restful-web-services/src/main/java/com/example/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/example/restfulwebservices/user/User.java
@@ -1,15 +1,26 @@
 package com.example.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+//@JsonIgnoreProperties(value={"id", "name"}) ... also works
+@ApiModel(description="All details about the user.")
 public class User {
 
+    @JsonIgnore
     private Integer id;
+
     @Size(min=2, message="Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should have at least 2 characters.")
     private String name;
     @Past
+    @ApiModelProperty(notes = "Birthday cannot be in the past.")
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
